d263c0aea5c1634f9da4fe84b996506b257f578e
soyreynald <reynaldramirez@hotmail.com>
2024-08-28 20:17:07 -0400

    [U] Update to show the guys the code of the app of the group 2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Forza Motosport Rent-car
 Project developed to UNICARIBE. It was suppose to be Private, but here where I am living, the money they count it with extremely measurement. ANYWAY: This project is going to be amazing to the glory of Jesus (John 14:15-31).
+
+## .. To execute the app:
+1. You need to have installed npm or Node.JS
+2. AFTER installing it, just run the server and you will see the app on MOBILE and Web
+
+# Command with NodeJS:
+http-server -p <PORT #>
+
+... Ejemplo:
+http-server -p 3340
---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html' ,
+                '/font' ,
+                '/app.js' ,
+                '/manifest.json' ,
+                '/img/favicon/android-chrome-512x512.png',
+				'/package.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
