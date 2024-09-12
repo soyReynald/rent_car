3617938443398ef9301ea7f29338447ef01d16fc
soyreynald <reynaldramirez@hotmail.com>
2024-09-12 18:59:44 -0400

    [U - Isaiah 50:8, Luke 6:29] Finally showing image of a car retrieved from an API

--- testAPI.js
+++ testAPI.js
@@ -12,9 +12,14 @@ async function trowResult(url, options) {
     try {
         const response = await fetch(url, options);
         const result = await response.text();
-        // var carChose = JSON.stringify(JSON.parse(result)[3]); // sample purposes
+        //  var carChose = JSON.stringify(JSON.parse(result)[3]); // sample purposes
         var carArray = JSON.parse(result);
-        document.querySelector("#cars_list_container").innerHTML = "<li>"+carArray[3].make+"</li>";
+        console.log(carArray);
+        let imageFolder = "./img/cars/";
+        const imageFiles = [carArray[3].make+"_"+carArray[3].year+".jpg", carArray[5].make+"_"+carArray[5].year+".jpg", carArray[8].model+"_"+carArray[8].year+"_suv"+".jpg"];
+        imageFiles.forEach((car, idx) => {
+            document.querySelector(".image-list").innerHTML = "<img class='image-item' src="+imageFolder+car.toLowerCase()+" alt='img-{idx}'/>";
+        });
     } catch (error) {
         console.error(error);
     }
